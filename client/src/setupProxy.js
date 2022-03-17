557c936985d6c29e36dadf39f43b2edcb98f5037
김종석 <kimjongseok@bouncecode.io>
2022-03-17 18:20:06 +0900

    중간 점검 완료

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,24 +3,24 @@ const httpProxy = require("http-proxy");
 
 module.exports = function(app) {
 
-  if(process.env.NODE_ENV === 'production') {
-    app.use(
-      createProxyMiddleware('/api',{
-        target: 'https://git.heroku.com/catting-system.git:7000',
-        changeOrigin: true,
-      }),
+//   if(process.env.NODE_ENV === 'production') {
+//     app.use(
+//       createProxyMiddleware('/api',{
+//         target: 'https://git.heroku.com/catting-system.git:7000',
+//         changeOrigin: true,
+//       }),
       
-    );
+//     );
   
-    app.use(
-      createProxyMiddleware('/socket.io', {
-        target: 'https://git.heroku.com/catting-system.git:7001',
-        changeOrigin: true,
-        // ws: true, 
-        logLevel: 'debug',
-      })
-    )
-}
+//     app.use(
+//       createProxyMiddleware('/socket.io', {
+//         target: 'https://git.heroku.com/catting-system.git:7001',
+//         changeOrigin: true,
+//         // ws: true, 
+//         logLevel: 'debug',
+//       })
+//     )
+// }
   app.use(
     createProxyMiddleware('/api',{
       target: 'http://localhost:7000',
