1887884de5708c01d9fea930fc9601b4806c7679
김종석 <kimjongseok@bouncecode.io>
2022-03-18 09:21:45 +0900

    edit Proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const httpProxy = require("http-proxy");
 
 module.exports = function(app) {
 
-  if(process.env.NODE_ENV === 'production') {
+
     app.use(
       createProxyMiddleware('/api',{
         target: 'https://catting-system.herokuapp.com:7000',
@@ -20,22 +20,24 @@ module.exports = function(app) {
         logLevel: 'debug',
       })
     )
-}
-  app.use(
-    createProxyMiddleware('/api',{
-      target: 'http://localhost:7000',
-      changeOrigin: true,
-    }),
-    
-  );
 
-  app.use(
-    createProxyMiddleware('/socket.io', {
-      target: 'http://localhost:7001',
-      changeOrigin: true,
-      // ws: true, 
-      logLevel: 'debug',
-    })
-  )
+
+
+  // app.use(
+  //   createProxyMiddleware('/api',{
+  //     target: 'http://localhost:7000',
+  //     changeOrigin: true,
+  //   }),
+    
+  // );
+
+  // app.use(
+  //   createProxyMiddleware('/socket.io', {
+  //     target: 'http://localhost:7001',
+  //     changeOrigin: true,
+  //     // ws: true, 
+  //     logLevel: 'debug',
+  //   })
+  // )
 
 };
