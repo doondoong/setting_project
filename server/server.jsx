c7dfd6e0c2ed9b74e24fac843d834032421641c0
김종석 <kimjongseok@bouncecode.io>
2022-03-21 17:42:14 +0900

    npm으로 교체

--- server/server.jsx
+++ server/server.jsx
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const {auth} = require('./middleware/auth')
 const path = require('path')
-
+console.log("Database_URL", process.env.DATABASE_URL);
 if(process.env.NODE_ENV === 'production') {
     server.use(express.static('client/build'));
     server.get('*', (req,res) => {
