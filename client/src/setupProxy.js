f4e83ddfed7dc2d002136b8ee53a353fe61c3413
김종석 <kimjongseok@bouncecode.io>
2022-03-22 16:21:38 +0900

    asdf

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,16 +1,4 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-// const httpProxy = require("http-proxy");
-
-
-// module.exports = function (app) {
-//   app.use(
-//       '/api',
-//       createProxyMiddleware({
-//           target: 'https://chatting-system-no3.herokuapp.com/',
-//           changeOrigin: true,
-//       })
-//   );
-// };
 
 module.exports = function(app) {
 
@@ -22,15 +10,14 @@ if(process.env.NODE_ENV === 'production') {
     }),
   );
 
-
-  // app.use(
-  //   createProxyMiddleware('/socket.io', {
-  //     target: 'https://chatting-system-no3.herokuapp.com/',
-  //     changeOrigin: true,
-  //     ws: true, 
-  //     logLevel: 'debug',
-  //   })
-  // )
+  app.use(
+    createProxyMiddleware('/socket.io', {
+      target: 'https://chatting-system-no3.herokuapp.com/',
+      changeOrigin: true,
+      ws: true, 
+      logLevel: 'debug',
+    })
+  )
 
 } else {
 
@@ -41,14 +28,14 @@ if(process.env.NODE_ENV === 'production') {
     }),
   );
 
-  // app.use(
-  //   createProxyMiddleware('/socket.io', {
-  //     target: 'http://localhost:7001',
-  //     changeOrigin: true,
-  //     ws: true, 
-  //     logLevel: 'debug',
-  //   })
-  // )
+  app.use(
+    createProxyMiddleware('/socket.io', {
+      target: 'http://localhost:7001',
+      changeOrigin: true,
+      ws: true, 
+      logLevel: 'debug',
+    })
+  )
 
 }
 
